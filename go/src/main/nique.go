0f2600e9f232d412d58e25d71979057251a12e9f
abouyx <antoine.bouyx@altran.com>
2019-05-14 14:16:07 +0200

    nique

--- go/src/main/nique.go
+++ go/src/main/nique.go
@@ -5,6 +5,6 @@ import (
 	"net/http"
 	"time"
 )
-func Time(w http.ResponseWriter, r *http.Request) {
+func Nique(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, time.Now().Format(time.RFC1123Z))
 }
