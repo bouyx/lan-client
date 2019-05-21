fdb1bc0950cd99a6b3d2dc4272862adf3315eb55
abouyx <antoine.bouyx@altran.com>
2019-05-21 17:27:08 +0200

    we finally use react

--- next/pages/index.js
+++ next/pages/index.js
@@ -1,26 +1,33 @@
+import React from 'react'
 import fetch from 'isomorphic-unfetch'
 
-const Index = props => (
-  <div>
-    <div>
-      <button>create</button>
-    </div>
-    <div>
-      <button>details</button>
-    </div>
-    <div>
-      <button onClick={props.prout}>prout</button>
-      <div>prout ={props.pets}</div>
-
-
-    </div>
-  </div>
-)
-Index.prout = async function () {
-  console.log("a kadok")
+const fetchPets = async () => {
+  console.log('a kadok')
   const res = await fetch(`https://lanmanager.antoinebouyx.now.sh/go/prout`)
   const pets = await res.json()
   return { pets }
+}
+
+function Index() {
+  const [pets, setPets] = React.useState()
+  const handleOnClick = async () => {
+    const _pets = await fetchPets()
+    setPets(_pets)
+  }
+  return (
+    <div>
+      <div>
+        <button>create</button>
+      </div>
+      <div>
+        <button>details</button>
+      </div>
+      <div>
+        <button onClick={handleOnClick}>prout</button>
+        <pre>{JSON.stringify(pets, null, 2)}</pre>
+      </div>
+    </div>
+  )
 }
 
 export default Index
